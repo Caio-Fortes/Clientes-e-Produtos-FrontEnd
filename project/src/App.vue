4791bb3ba8895a44ef66b26263354316f41fa3e7
Caio <caiofortesprofissional@gmal.com>
2024-08-11 09:51:52 -0300

    configuração para buscar os dados

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,5 +1,26 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import PostService from "./api/PostService";
+</script>
+
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  },
+  mounted(){
+    this.getAllClientes();
+  },
+  methods:{
+    async getAllClientes(){
+      const teste = await PostService.getPosts("http://localhost:8081/clientes");
+      console.log(teste)
+    }
+  }
+}
+
 </script>
 
 <template>
---
+++ project/src/api/PostService.js
@@ -0,0 +1,41 @@
+class PostService {
+    static getPosts(url) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await fetch(url, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                });
+                if (!res.ok) {
+                    throw new Error('Network response was not ok ' + res.statusText);
+                }
+                const data = await res.json();
+                resolve(data);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    static async insertPost(url, params) {
+        try {
+            const res = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(params)
+            });
+            if (!res.ok) {
+                throw new Error('Network response was not ok ' + res.statusText);
+            }
+            return await res.json();
+        } catch (err) {
+            throw new Error('Failed to insert post: ' + err.message);
+        }
+    }
+}
+
+export default PostService;
