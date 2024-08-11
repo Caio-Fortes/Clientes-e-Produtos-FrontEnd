20db01673d018e3f4a65480e96e9ce48f17a3d38
Caio <caiofortesprofissional@gmal.com>
2024-08-11 17:52:07 -0300

    limpando solução

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import PostService from "./api/PostService";
-import Header from "./components/templates/Header.vue";
+import Header from "./components/Header.vue";
 </script>
 
 <script>
