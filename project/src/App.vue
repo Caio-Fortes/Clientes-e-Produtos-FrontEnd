6456d8833a1de223e00434c61e086078e600ed52
Caio <caiofortesprofissional@gmal.com>
2024-08-11 14:46:17 -0300

    header

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import PostService from "./api/PostService";
+import Header from "./components/templates/Header.vue";
 </script>
 
 <script>
@@ -24,14 +25,10 @@ export default {
 </script>
 
 <template>
-  <header>
-      <nav>
-        <!-- <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink> -->
-      </nav>
-  </header>
-  <h1>Lista de Clientes</h1>
-  <h3>dadasdas</h3>
-  <p>dasdasdasdasdasdasdasdasdas</p>
+  <Header />
+    <!-- <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav> -->
   <RouterView />
 </template>
---
+++ project/src/components/templates/Header.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+}
+
+</script>
+
+<template>
+   <header>
+        <div id="headerTemplate">
+            <div class="logo-header">
+                <img src="/logo.png" />
+            </div>
+            <div id="headerTools">
+                <div class="user-icon">
+                    <img src="/icons/usuario.png" />
+                </div>
+                <div class="user-login">
+                    <div class="user-name">Usuário</div>
+                    <div class="user-position">Administrador</div>
+                </div>
+                <div class="icon-loggout">
+                    <img src="/icons/sair.png" />
+                </div>
+            </div>
+        </div>
+  </header>
+</template>
+
+<script setup>
+
+</script>
